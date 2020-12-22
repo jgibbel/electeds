b703cafab96fa4a495c244f90f73ba1bba965a85
jgibbel <jamesgibbel@gmail.com>
2020-12-21 19:33:59 -0500

    typos

--- src/CityCouncilShow.js
+++ src/CityCouncilShow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CandidateCard from './CandidateCard';
+import './App.css';
 
 import { Link } from 'react-router-dom';
 
--- src/Contribute.js
+++ src/Contribute.js
@@ -4,8 +4,8 @@ class Contribute extends Component {
     render() {
         return (
             <div>
-                <p>ReppingNYC is currently focused on inputting every city council candidate into the database. The next leg of the project is to send a slate of questions to each candidate and host their answers on the site. The site is under construction. Info sitting elected officials is out of date as of the 2020 election.</p>
-                <p>Repping is operated by JG. you can find me on ig @dr_ipping, twitter @userspo, and much of the coding and data entry is livestreamed at <a href="twitch.tv/reppingnyc" target="_blank" rel="noopener noreferrer" className="textlink">twitch.tv/reppingnyc</a>. Please come and say hi if you don't know me, I'd love to make your acquaintance. Additionally, reach me via email: reppingnyc@gmail.com</p>
+                <p>ReppingNYC is currently focused on inputting every city council candidate into the database. The next leg of the project is to send a slate of questions to each candidate and host their answers on the site. The site is under construction. Info on sitting elected officials is out of date as of the 2020 election.</p>
+                <p>Repping is operated by JG. You can find me on ig @dr_ipping, twitter @userspo, and much of the coding and data entry is livestreamed at <a href="twitch.tv/reppingnyc" target="_blank" rel="noopener noreferrer" className="textlink">twitch.tv/reppingnyc</a>. Please come and say hi if you don't know me, I'd love to make your acquaintance. Additionally, reach me via email: reppingnyc@gmail.com</p>
             </div>
         );
     }
